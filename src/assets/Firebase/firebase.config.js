e3b505e5dbe609f4b49ea77211fd9f6da0af68f7
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-12-21 23:46:46 +0600

    Dependency Installed

---
+++ src/assets/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjUMP8rVCTcVpZxFXgvTTE0hpiotmuDM0",
+  authDomain: "zenbyte-1d2a2.firebaseapp.com",
+  projectId: "zenbyte-1d2a2",
+  storageBucket: "zenbyte-1d2a2.appspot.com",
+  messagingSenderId: "659350234039",
+  appId: "1:659350234039:web:f2955277a42c2c3bc97e32"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
