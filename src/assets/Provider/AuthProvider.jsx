40ebdc4944091eb7bd206d29b54b454dedc0d954
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-12-27 23:31:02 +0600

    AuthProvider

--- src/assets/Provider/AuthProvider.jsx
+++ src/assets/Provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useHistory, useLocation } from 'react-router-dom';
+
 import React, { createContext, useEffect, useState } from 'react';
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import app from '../Firebase/firebase.config';
